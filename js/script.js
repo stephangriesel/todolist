3b8b7c5d61139bf9cfecafe684f745870de23c6e
Stephan Griesel <sgriesel@gmail.com>
2017-03-29 10:01:51 +0200

    refactoring code

--- js/script.js
+++ js/script.js
@@ -62,11 +62,23 @@ var todoList = {
   }
 };
 
+/* Old code before refactoring
 var displayTodosButton = document.getElementById("displayTodosButton");
 var toggleAllButton = document.getElementById("toggleAllButton");
+
 displayTodosButton.addEventListener("click", function() {
   todoList.displayTodos();
 });
+
 toggleAllButton.addEventListener("click", function() {
  todoList.toggleAll();
-});
+}); */
+
+var handlers = {
+  displayTodos: function() {
+    todoList.displayTodos();
+},
+  toggleAll: function() {
+    todoList.toggleAll();
+  }
+};

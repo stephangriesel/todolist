dd37457acfa7f781e92991ff05d2e0fc432f30af
Stephan Griesel <sgriesel@gmail.com>
2017-03-31 10:47:54 +0200

    toggle completed button and function added

--- js/script.js
+++ js/script.js
@@ -66,9 +66,6 @@ var todoList = {
 var handlers = {
   displayTodos: function() {
     todoList.displayTodos();
-},
-  toggleAll: function() {
-    todoList.toggleAll();
   },
   addTodo: function () {
     var addTodoTextInput = document.getElementById("addTodoTextInput");
@@ -82,9 +79,18 @@ var handlers = {
     changeTodoPositionInput.value = '';
     changeTodoTextInput.value = "Add some more?";
   },
-  deleteTodo: function(){
+  deleteTodo: function() {
     var deleteTodoPositionInput = document.getElementById("deleteTodoPositionInput");
     todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
     deleteTodoPositionInput.value = "";
+  },
+  toggleCompleted: function() {
+    var toggleCompletedPositionInput = document.getElementById("toggleCompletedPositionInput");
+    todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+    toggleCompletedPositionInput.value="";
+  },
+    toggleAll: function() {
+    todoList.toggleAll();
   }
+    
 };

1d6bba2e835f8c2e794fddabee948f8eea21b07f
Stephan Griesel <sgriesel@gmail.com>
2017-04-03 14:45:15 +0200

    add li element for every todo

--- js/script.js
+++ js/script.js
@@ -92,5 +92,15 @@ var handlers = {
     toggleAll: function() {
     todoList.toggleAll();
   }
-    
+};
+
+var view = {
+  displayTodos: function() {
+    var todosUl = document.querySelector("ul");
+    todosUl.innerHTML = "";
+    for (var i = 0; i < todoList.todos.length; i++) {
+      var todoLi = document.createElement("li");
+      todosUl.appendChild(todoLi);
+    }
+  }
 };
